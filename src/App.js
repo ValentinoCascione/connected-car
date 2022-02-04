5be68fc46197efee89791742b61bd052f0e4f347
“ValentinoCascione” <v.cascione@it-students.fr>
2022-02-04 15:44:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Create WebSocket connection.
+  const socket = new WebSocket('ws://192.168.1.210:4040');
+  console.log(socket);
+
+  // Connection opened
+  socket.addEventListener('open', function (event) {
+      console.log('iciicici')
+      socket.send('Yo')
+      console.log('Connected to the WS Server!')
+  });
+
+  // Listen for messages
+  socket.addEventListener('message', function (event) {
+      console.log('Message from server ', event.data);
+  });
+
+  const sendForward = () => {
+      socket.send('forward')
+  }
+
+  const sendReverse = () => {
+      socket.send('reverse')
+  }
+
+  const sendTurnLeft = () => {
+      socket.send('turnLeft')
+  }
+
+  const sendTurnRight = () => {
+      socket.send('turnRight')
+  }
+
+  const stop = () => {
+    socket.send('stop')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='button' id="turnLeftButton" onTouchEnd={() => stop()} onTouchStart={() => sendTurnLeft()}>Left</button>
+      <div id="ForwardReverseButtons">
+        <button className='button' id="forwardButton" onTouchEnd={() => stop()} onTouchStart={() => sendForward()}>Forward</button>
+        <button className='button' id="reverseButon" onTouchEnd={() => stop()} onTouchStart={() => sendReverse()}>Reverse</button>
+      </div>
+      <button className='button' id="turnRightButton" onTouchEnd={() => stop()} onTouchStart={() => sendTurnRight()}>Right</button>
     </div>
   );
 }
